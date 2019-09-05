77eee145fd6ad06f000192f27b36fc020e0f517a
ZheqiWang <wangzheqi130@gmail.com>
2019-09-05 18:57:52 -0500

    Add files via upload

---
+++ 2019.8.29.R
@@ -0,0 +1,26 @@
+# R is case sensitive
+# Expressions are grouped with braces
+# Parenthesis are used for function
+# Brackets are used for vectors, matrices, ...
+# to run a single line :command + enter
+
+# assign 1 to variable c
+c <- 1 
+ls() # function to return things saved in the environment
+?ls # get information of this function
+?str
+str(c)
+?rm
+rm(c)
+
+# create a vector
+vec2 <- c(1,10,11)
+vec <- 10:15
+
+ls()
+?ls
+?str
+str(c)
+str(vec)
+?rm
+rm(c)
